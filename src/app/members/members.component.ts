f351ae5d934170675476a02513c6ffa04bbe665f
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-16 13:23:58 -0800

    linked ot the three new generated post routes/ components

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,34 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-
-import { PostsService } from '../services/posts.service';
-import { Post } from '../post'
-
-
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css'],
-  providers: [ PostsService]
+  styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
 	@Input() user;
-	posts: Array<Post>;
+	
 
-  	constructor( private postsService: PostsService ) { }
+  	constructor(  ) { }
 
   	ngOnInit() {
-  		this.getPosts();
+
   	}
 
-  	getPosts() {
-  		this.postsService.getPosts()
-  			.subscribe((res) => {
-  				if ( res.status === 'success') {
-  					this.posts = res.posts
-  					console.log(res);
-  				}
-  			})
-  	}
+  	
 
 }
