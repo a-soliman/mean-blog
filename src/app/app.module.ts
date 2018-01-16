71d7e5a4473970160c146d0a800897e658d4991b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-16 13:24:17 -0800

    used the new route plan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,9 @@ import { NavComponent } from './nav/nav.component';
 import { MembersComponent } from './members/members.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { PostsComponent } from './posts/posts.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
 
 
 @NgModule({
@@ -19,13 +23,17 @@ import { LoginComponent } from './login/login.component';
     NavComponent,
     MembersComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    PostsComponent,
+    AddPostComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    routes
   ],
   providers: [SimpleService],
   bootstrap: [AppComponent]
