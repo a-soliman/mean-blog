b58de19f68860e424286440a36847db29054abc8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-16 11:13:05 -0800

    coded posts service

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, ResponseOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class PostsService {
 
-  constructor() { }
+	constructor( private http: Http ) { }
+
+	getPosts() {
+		return this.http.get("/api/posts")
+			.map( res => res.json());
+	}
 
 }
