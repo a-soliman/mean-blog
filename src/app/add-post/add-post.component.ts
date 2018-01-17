c386aae6a88eb46f5a2f58c43cdd71e9fb5c5ec8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-17 13:32:52 -0800

    modified the add-post component to retrive the categories list from the server #onInit#

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -2,24 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AddPostService } from '../services/add-post.service'
+import { AddPostService } from '../services/add-post.service';
+import { AddCategoryService } from '../services/add-category.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.css'],
-  providers: [ AddPostService ]
+  providers: [ AddPostService, AddCategoryService ]
 })
 export class AddPostComponent implements OnInit {
 
-	addPostForm: FormGroup;
-	serverValidationErrors: Array<any> = [];
-	successMessage: string;
-	user = JSON.parse(localStorage.getItem('user')) || null;
+	addPostForm              : FormGroup;
+  categories               : Array<any> = [];
+	serverValidationErrors   : Array<any> = [];
+	successMessage           : string;
+	user                     : User;
 
   	constructor( private fb: FormBuilder, 
   				 private router:Router,
-  				 private addPostService: AddPostService 
+  				 private addPostService: AddPostService,
+           private addCategoryService: AddCategoryService 
   				) { 
   		this.addPostForm = fb.group({
   			'title': [null, Validators.compose([ Validators.required, Validators.minLength(3) ])],
@@ -31,11 +35,13 @@ export class AddPostComponent implements OnInit {
   	}
 
   	ngOnInit() {
-  		console.log(this.user)
-  		if( this.user === null ) {
-  			console.log('user is null')
+      this.user = JSON.parse(localStorage.getItem('user')) || null;
+  		
+      if( this.user === null ) {
   			this.router.navigate([" "])
   		}
+
+      this.getCategories();
   	}
 
   	addPost( post ) {
@@ -48,11 +54,25 @@ export class AddPostComponent implements OnInit {
 					this.serverValidationErrors = [];
 					this.addPostForm.reset();
 					this.router.navigate(["/posts"])
-				}
-				if ( res.errors) {
-					return this.serverValidationErrors = res.errors;
-				}
+				  }
+				  
   			})
   	}
+
+    getCategories() {
+      this.addCategoryService.getCategories()
+        .subscribe((res) => {
+
+          if ( res.errors) {
+            return this.serverValidationErrors = res.errors;
+          }
+
+          if ( res.success === true ) {
+              res.categories.forEach((category) => {
+              this.categories.push(category.name);
+            })
+          }
+        })
+    }
 
 }
