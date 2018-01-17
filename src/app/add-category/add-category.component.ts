6814577717923491cf31cd2acfdcbc966a6aae85
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-17 13:33:43 -0800

    coded add-category componentn to post a new component to the server => DB

--- src/app/add-category/add-category.component.ts
+++ src/app/add-category/add-category.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AddCategoryService } from '../services/add-category.service'
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.css']
+  styleUrls: ['./add-category.component.css'],
+  providers: [ AddCategoryService ]
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+	addCategoryForm: FormGroup;
+	serverValidationErrors: Array<any> = [];
+	successMessage: string;
 
-  ngOnInit() {
-  }
+  	constructor( private addCategoryService: AddCategoryService,
+  				 private router: Router,
+  				 private fb: FormBuilder ) { 
+
+  		this.addCategoryForm = fb.group({
+  			'name': [ null, Validators.compose([ Validators.required, Validators.minLength(3) ])]
+  		});
+  	}
+
+  	ngOnInit() {
+  	}
+
+  	addCategory( category ) {
+  		this.addCategoryService.addCategory(category)
+  			.subscribe((res) => {
+  				if ( res.success === true) {
+					this.successMessage = res.message;
+					this.serverValidationErrors = [];
+					this.addCategoryForm.reset();
+					this.router.navigate(["/posts"])
+				}
+				if ( res.errors) {
+					return this.serverValidationErrors = res.errors;
+				}
+  			})
+  	}
 
 }
