3e08f6a63700971a05ac637213a7b3377423830b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-17 09:28:05 -0800

    coded the form and the post request && form validation for add post route

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AddPostService } from '../services/add-post.service'
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
+  providers: [ AddPostService ]
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+	addPostForm: FormGroup;
+	serverValidationErrors: Array<any> = [];
+	successMessage: string;
+	user = JSON.parse(localStorage.getItem('user')) || null;
 
-  ngOnInit() {
-  }
+  	constructor( private fb: FormBuilder, 
+  				 private router:Router,
+  				 private addPostService: AddPostService 
+  				) { 
+  		this.addPostForm = fb.group({
+  			'title': [null, Validators.compose([ Validators.required, Validators.minLength(3) ])],
+  			'category': [null],
+  			'body': [ null, Validators.compose([ Validators.required, Validators.minLength(20) ])],
+  			'mainImage': [ null],
+  			'author': [ null ]
+  		});
+  	}
+
+  	ngOnInit() {
+  		console.log(this.user)
+  		if( this.user === null ) {
+  			console.log('user is null')
+  			this.router.navigate([" "])
+  		}
+  	}
+
+  	addPost( post ) {
+  		post.author = this.user.username;
+  		this.addPostService.addPost(post)
+  			.subscribe((res) => {
+  				console.log(res)
+  				if ( res.success === true) {
+					this.successMessage = res.message;
+					this.serverValidationErrors = [];
+					this.addPostForm.reset();
+					this.router.navigate(["/posts"])
+				}
+				if ( res.errors) {
+					return this.serverValidationErrors = res.errors;
+				}
+  			})
+  	}
 
 }
