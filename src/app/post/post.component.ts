be43edf6bf9565a832e9230b4ac1ad3ac0318b18
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-17 14:50:10 -0800

    coded a post component to handle the view of a single post

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PostService } from '../services/post.service';
+import { Post } from '../post';
+
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+  providers: [ PostService ]
+})
+
+export class PostComponent implements OnInit {
+
+	post: Post;
+
+  	constructor( private route: ActivatedRoute, 
+  				 private postService: PostService ) { }
+
+  	ngOnInit() {
+  		this.route.params.subscribe(params => {
+  			console.log(params.id)
+  			this.getPost(params.id);
+  		})
+  	}
+
+  	getPost( id ) {
+  		this.postService.getPost(id)
+  			.subscribe((res) => {
+  				this.post = res.post
+  				console.log(res.post)
+  			})
+  	}
+
+
+
+}
