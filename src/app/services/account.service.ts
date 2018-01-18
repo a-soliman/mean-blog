08addfe239c56f1f1c1591d57716cb4a4f3f333e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-18 13:28:17 -0800

    created a new service to handle the user account

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, ResponseOptions } from '@angular/http';
+
+@Injectable()
+export class AccountService {
+
+  constructor( private http: Http ) { }
+
+  
+
+}
