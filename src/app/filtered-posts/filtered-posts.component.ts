b850ce91ff5de1335966734a8efe75723e590003
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-18 09:14:54 -0800

    coded the logic for filtering posts

--- src/app/filtered-posts/filtered-posts.component.ts
+++ src/app/filtered-posts/filtered-posts.component.ts
@@ -1,21 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { PostsService } from '../services/posts.service';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-filtered-posts',
   templateUrl: './filtered-posts.component.html',
-  styleUrls: ['./filtered-posts.component.css']
+  styleUrls: ['./filtered-posts.component.css'],
+  providers: [ PostsService ]
 })
+
 export class FilteredPostsComponent implements OnInit {
 
-	filterBy: string;
+	  filter_by: string;
+    filter   : string
+    posts    : Array<Post>;
 
-  	constructor( private route: ActivatedRoute ) { }
+  	constructor( private route: ActivatedRoute,
+                 private postsService: PostsService ) { }
 
   	ngOnInit() {
   		this.route.params.subscribe(params => {
-  			console.log(params.filter);
+        this.filter    = params.filter;
+        this.filter_by = params.filter_by;
+        
+        if (this.filter_by === 'author') {
+          this.getPostsByAuthor(this.filter);
+        }
+        else {
+          this.getPostsByCategory(this.filter);
+        }
+
   		})
+
   	}
+
+    getPostsByCategory( category: string ) {
+      this.postsService.getPostsByCategory(category)
+        .subscribe((res) => {
+          if ( res.success === true ) {
+            this.posts = res.posts;
+          }
+        })
+    }
+
+    getPostsByAuthor ( author: string ) {
+      this.postsService.getPostsByAuthor(author)
+        .subscribe( (res) => {
+          if ( res.success === true ) {
+            this.posts = res.posts;
+          }
+        })
+    }
 
 }
